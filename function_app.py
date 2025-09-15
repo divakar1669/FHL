541fc8defcea4822b8c55e4483b9c62c7e864ddd
Divakar T <106461774+divakar1669@users.noreply.github.com>
2025-09-15 23:17:56 +0530

    initial commit

--- function_app.py
+++ function_app.py
@@ -20,6 +20,6 @@ def cri_hack(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "good day !!!",
              status_code=200
         )
