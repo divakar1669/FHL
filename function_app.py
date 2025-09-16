fc58ab5746b3aa012f7d0a0ac46ce5ad85afe243
Divakar T <106461774+divakar1669@users.noreply.github.com>
2025-09-16 08:40:11 +0530

    test changes

--- function_app.py
+++ function_app.py
@@ -23,3 +23,40 @@ def cri_hack(req: func.HttpRequest) -> func.HttpResponse:
              "good day !!!",
              status_code=200  
         )
+    
+@app.route(route="subscribe")
+def subscribe(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    email = req.params.get('email')
+    tags = req.params.get('tags') # this must be a comma-separated string of tags 
+    if not email:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            email = req_body.get('email')
+
+    if email:
+        return func.HttpResponse(f"Subscription successful for {email}.")
+    else:
+        return func.HttpResponse(
+            "Please provide a valid email address.",
+            status_code=400
+        )
+
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "good day !!!",
+             status_code=200  
+        )
